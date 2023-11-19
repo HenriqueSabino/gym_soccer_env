804380e878ad4910c5aea3bed2f26a63f439d80e
VictorG-028 <victor6g0@gmail.com>
2023-11-19 16:44:20 -0300

    Update order of play in PlayerSelector

--- env/player_selection.py
+++ env/player_selection.py
@@ -6,9 +6,12 @@ TEAM_NAMES = ["right_team", "left_team"]
 
 class PlayerSelector:
     def __init__(self, player_names: list[str]):
-        self.player_names = player_names
+
+        # Sort player names in the pós kickof order of turns
+        self.player_order_to_play = [player_names[i//2] if i % 2 == 0 else player_names[i//2 + 11] for i in range(22)]
+
         self._index = 0
-        self._current_player_name = self.player_names[self._index]
+        self._current_player_name = self.player_order_to_play[self._index]
         self._direction = np.array([1, 1])
         self._is_left_team = True
         self._is_left_team_str = "left_team"
@@ -32,24 +35,21 @@ class PlayerSelector:
         Pass the turn to next player and updates the 
         internal _current_player_name, _is_left_team and _direction info.
         """
-        # TODO: verificar a lógica que seleciona o próximo player para jogar 
-        # # alterna entre o time 1 e time 2 ?
-        # # é do mesmo time até cada um dos 11 agir ?
-        # # Lógica implementada abaixo:
-        # # Apenas time "left" joga até acontecer kick off. 
-        # # 11 do time "left" jogam, depois 11 do time "right" jogam
         if self._kickoff == True:
             self._index = (self._index + 1) % PLAYER_COUNT
-        else:
-            self._index = (self._index + 1) % (PLAYER_COUNT - 11)
-            
-        self._current_player_name = self.player_names[self._index]
-        if self._index in (11, 0) and self._kickoff == True:
+            self._current_player_name = self.player_order_to_play[self._index]
             self._toggle_side()
-            print("Trocou time")
+        else:
+            # if all player of left team should play before kickoff, then uncomment this line 
+            # self._index = (self._index + 2) % PLAYER_COUNT
+
+            # First left team player always play before kickof
+            self._current_player_name = self.player_order_to_play[self._index]
+               
         return self._current_player_name
 
     def _toggle_side(self):
         self._direction = self._direction * TOGGLE_X_ONLY
         self._is_left_team ^= True # is_left_side XOR True
         self._is_left_team_str = TEAM_NAMES[+(self._is_left_team)]
+        print("Trocou time")
